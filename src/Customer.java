54dd8f05a421300ba41a8e70a2b8a1cee5262335
Bernado, Douglas L. <bernadodouglas634@gmail.com>
2023-01-03 16:15:47 +0800

    File is missing

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,37 @@
-public class Customer
-{
+public class Customer extends Person {
+    private int CustomerNumber;
+    private boolean IsMailingList;
+
+    public void setCustomerNumber(int customerNumber) {
+        CustomerNumber = customerNumber;
+    }
+
+    public int getCustomerNumber() {
+        return CustomerNumber;
+    }
+
+    public void setMailingList(boolean mailingList) {
+        IsMailingList = mailingList;
+    }
+
+    public String getMailingList() {
+        String str;
+        if (IsMailingList == true)
+            str = "Yes";
+        else
+            str = "No";
+        return str;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person:" +
+			"\nName: " + getName() +
+			"\nAddress: " + getAddress() +
+			"\nPhone: " + getPhone() +
+			"\nBirtday: " + getBirtday() +
+			"\nCustomerNumber: " + CustomerNumber +
+			"\nMailingList: " + getMailingList();
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,39 @@
-public class Person
-{
+public class Person {
+
+    private String name, address, phone, birtday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String a) {
+        address = a;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String p) {
+        phone = p;
+    }
+
+
+    public String getBirtday() {
+        return birtday;
+    }
+
+    public void setBirtday(String b) {
+        birtday = b;
+    }
+
+
 }
