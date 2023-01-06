3dfd77964786fd7badb4d6033742dc77d9c5bf3d
Douglas Bernado <116050331+Douglas09-oop@users.noreply.github.com>
2023-01-06 10:17:46 +0800

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,3 +1,49 @@
-public class Student
-{
+public class Quiz {
+    private int id;
+    private Student student;
+    private int totalScore;
+    private int maxScore;
+  
+    public Quiz(int id, Student student, int totalScore, int maxScore) {
+      this.id = id;
+      this.student = student;
+      this.totalScore = totalScore;
+      this.maxScore = maxScore;
+    }
+  
+    public double grade() {
+      return (double) this.totalScore / this.maxScore;
+    }
+  }
+
+public class Student {
+    //Data Members
+    private String name;
+    private String email;
+    
+    //Constructor
+    public Student() {
+        name = "Unassigned";
+        email = "Unassigned";
+    }
+
+    //Returns the email of this student
+    public String getEmail() {
+        return email;
+    }
+    
+    //Returns the name of this student
+    public String getName() {
+        return name;
+    }
+    
+    //Assigns the email of this student
+    public void setEmail(String address) {
+        email = address;
+    }
+    
+    //Assigns the name of this student
+    public void setName(String studentName) {
+        name = studentName;
+    }
 }
